90c0ac816f4a4369fd4c9230c5a1b21d2837a9fc
vlgreb <vlgrebennikov@mail.ru>
2021-03-06 17:38:05 +0300

    create cells and random moving

--- maze_generator.py
+++ maze_generator.py
@@ -11,6 +11,59 @@ pygame.init()
 sc = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
 
+
+class Cell:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+
+    def draw_current_cell(self):
+        x, y = self.x * TILE, self.y * TILE
+        pygame.draw.rect(sc, pygame.Color('saddlebrown'), (x + 2, y + 2, TILE - 2, TILE - 2))
+
+    def draw(self):
+        x, y = self.x * TILE, self.y * TILE
+        if self.visited:
+            pygame.draw.rect(sc, pygame.Color('black'), (x, y, TILE, TILE))
+
+        if self.walls['top']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y), (x + TILE, y), 2)
+        if self.walls['right']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y), (x + TILE, y + TILE), 2)
+        if self.walls['bottom']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y + TILE), (x, y + TILE), 2)
+        if self.walls['left']:
+            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y + TILE), (x, y), 2)
+
+
+    def check_cell(self, x, y):
+        find_index = lambda x, y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
+            return False
+        return grid_cells[find_index(x, y)]
+
+    def check_neighbors(self):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1)
+        right = self.check_cell(self.x + 1, self.y)
+        bottom = self.check_cell(self.x, self.y + 1)
+        left = self.check_cell(self.x - 1, self.y)
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+        return choice(neighbors) if neighbors else False
+
+
+grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
+current_cell = grid_cells[0]
+stack = []
+
 while True:
     sc.fill(pygame.Color('darkslategray'))
 
@@ -18,7 +71,16 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
+    [cell.draw() for cell in grid_cells]
+    current_cell.vizited = True
+    current_cell.draw_current_cell()
+
+    next_cell = current_cell.check_neighbors()
+    if next_cell:
+        next_cell.visited = True
+        current_cell = next_cell
+
     pygame.display.flip()
     clock.tick(30)
 
-# 1:06
+# 4:35
