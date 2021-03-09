5221fbb6ada19379dbd7177ee8d502832b86562d
vlgreb <vlgrebennikov@mail.ru>
2021-03-09 16:23:50 +0300

    create maze_generator

--- maze_generator.py
+++ maze_generator.py
@@ -3,7 +3,7 @@ from random import choice
 
 
 RES = WIDTH, HEIGHT = 1202, 902
-TILE = 100
+TILE = 50
 cols, rows = WIDTH // TILE, HEIGHT // TILE
 
 
@@ -60,9 +60,27 @@ class Cell:
         return choice(neighbors) if neighbors else False
 
 
+def remove_walls(current, next):
+    dx = current.x - next.x
+    if dx == 1:
+        current.walls['left'] = False
+        next.walls['right'] = False
+    elif dx == -1:
+        current.walls['right'] = False
+        next.walls['left'] = False
+    dy = current.y - next.y
+    if dy == 1:
+        current.walls['top'] = False
+        next.walls['bottom'] = False
+    elif dy == -1:
+        current.walls['bottom'] = False
+        next.walls['top'] = False
+
+
 grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
 current_cell = grid_cells[0]
 stack = []
+colors, color = [], 40
 
 while True:
     sc.fill(pygame.Color('darkslategray'))
@@ -74,13 +92,20 @@ while True:
     [cell.draw() for cell in grid_cells]
     current_cell.vizited = True
     current_cell.draw_current_cell()
+    [pygame.draw.rect(sc, colors[i], (cell.x * TILE + 5, cell.y * TILE + 5,
+                                      TILE - 10, TILE - 10), border_radius=12) for i, cell in enumerate(stack)]
 
     next_cell = current_cell.check_neighbors()
     if next_cell:
         next_cell.visited = True
+        stack.append(current_cell)
+        colors.append((min(color, 255), 10,  100))
+        color += 1
+        remove_walls(current_cell, next_cell)
         current_cell = next_cell
+    elif stack:
+        current_cell = stack.pop()
 
     pygame.display.flip()
     clock.tick(30)
 
-# 4:35
